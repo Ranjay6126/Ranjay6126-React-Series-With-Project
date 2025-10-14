7c668a4d8dc65a6ec34ff565192518f15db613ec
Ranjay6126 <6126ranjayprajapati@gmail.com>
2025-10-14 15:07:09 +0530

    Basic of React

---
+++ folder1/src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1> welcome to React</h1>
+      <h3>once look by inspect</h3>
+      Ranjay Prajapati // Gautam Prajapati
+    </div>
+  )
+}
+
+export default App
