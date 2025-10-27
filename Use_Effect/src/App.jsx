68451a0ed48bc609404d300c3fd085bff7707778
Ranjay6126 <6126ranjayprajapati@gmail.com>
2025-10-27 23:07:30 +0530

    Effect Handling Example2

--- Use_Effect/src/App.jsx
+++ Use_Effect/src/App.jsx
@@ -1,33 +1,71 @@
+// import { useEffect, useState } from "react"
+
 import { useEffect, useState } from "react"
+
+
+// const App = () => {
+
+//   const [num, setnum] = useState(0);
+
+//   const [first, setfirst] = useState(100)
+
+//   useEffect(function () {
+
+//   console.log('use effect is running..')
+
+//   },[num])
+
+//   return (
+//     <div>
+//       <h1>The value of Num1 is{num}</h1>
+//       <h2>The value of Num2 is{first}</h2>
+//       <button
+//        onMouseEnter={()=>{
+//         setnum(num+1) 
+//         }}
+//         onMouseLeave={()=>{
+//           setfirst(first+100)
+//         }}
+
+//         > Click me</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
 
 
 const App = () => {
 
-  const [num, setnum] = useState(0);
+  const [a, setA] = useState(0)
+  const [b, setB] = useState(-1)
 
-  const [first, setfirst] = useState(100)
+  useEffect(function (){
+  console.log("A is running");
+  },[a])
 
-  useEffect(function () {
 
-  console.log('use effect is running..')
+  useEffect(function(){
+    console.log("B is running");
+  },[b])
 
-  },[num])
 
   return (
     <div>
-      <h1>The value of Num1 is{num}</h1>
-      <h2>The value of Num2 is{first}</h2>
-      <button
-       onMouseEnter={()=>{
-        setnum(num+1) 
-        }}
-        onMouseLeave={()=>{
-          setfirst(first+100)
-        }}
+      <h1>{a}</h1>
+    <button onClick={()=>{
+      setA(a+1)
+    }}>Click A</button>
 
-        > Click me</button>
+      <h1>{b}</h1>
+      <button onClick={()=>{
+     setB(b-1)
+      }}>Click B</button>
     </div>
   )
 }
 
 export default App
+
